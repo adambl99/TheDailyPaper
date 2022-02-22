9bef697fdc21fec5302bf5f165131d3d3057bad0
Adaml <adam6925@stud.kea.dk>
2022-02-22 15:06:27 +0100

    TheDailyPaper

---
+++ src/test/java/com/example/thedailypaper/TheDailyPaperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thedailypaper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheDailyPaperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
